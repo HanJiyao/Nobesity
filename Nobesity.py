40efea3e700f5229767bdb2a2ad677239ed17dc0
H96Ycom <hjy199638@gmail.com>
2017-11-05 00:31:37 +0800

    1

--- Nobesity.py
+++ Nobesity.py
@@ -9,9 +9,39 @@ def home():
     return render_template('home.html')
 
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/signup')
+def timeline():
+    return render_template('timeline.html')
+
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+
 @app.route('/profile')
-def evaluation():
+def profile():
     return render_template('profile.html')
+
+
+@app.route('/plans')
+def plans():
+    return render_template('plans.html')
+
+
+@app.route('/diet')
+def diet():
+    return render_template('healthy_diet.html')
+
+
+@app.route('/quiz')
+def quiz():
+    return render_template('quiz.html')
 
 
 if __name__ == '__main__':
