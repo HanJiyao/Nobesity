9438b30b2334d2dad62dfb40785d1df828160a43
Tysonlkm <bladeliger09@gmail.com>
2018-01-29 10:37:23 +0800

    Fixed my diet bug

--- Nobesity.py
+++ Nobesity.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, flash, redirect, url_for, session
-from wtforms import Form, StringField, TextAreaField, RadioField, SelectField, PasswordField, DecimalField, \
+from wtforms import Form, StringField, TextAreaField, RadioField, SelectField, PasswordField, DecimalField,\
     IntegerField, DateField, validators, ValidationError
 import firebase_admin
 from firebase_admin import credentials, db
